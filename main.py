29ed133d43ef73497e76bfb7acb592ac96350f7a
lossi1997 <tuukka.orava@outlook.com>
2024-02-20 18:04:49 +0200

    separated node to its own file

--- main.py
+++ main.py
@@ -1,41 +1,8 @@
 
 from typing import Any
 
+from node import Node
 
-class Node:
-    def __init__(self, value: Any):
-        self.value = value
-        self.next = None
-
-    @staticmethod
-    def _get_value(other):
-        if isinstance(other, Node):
-            return other.value
-        else:
-            return other
-
-    def __int__(self):
-        return int(self.value)
-
-    def __str__(self) -> str:
-        return str(self.value)
-
-    def __add__(self, other):
-        return self.value + self._get_value(other)
-
-    def __sub__(self, other):
-        return self.value - self._get_value(other)
-
-    def __mul__(self, other):
-        return self.value * self._get_value(other)
-
-    def __truediv__(self, other):  # FIXME incorrect result when dividing multiple values at once
-        return self.value / self._get_value(other)
-
-    __radd__ = __add__
-    __rsub__ = __sub__
-    __rmul__ = __mul__
-    __rtruediv__ = __truediv__
 
 class Linkedlist:
     def __init__(self, *args):
