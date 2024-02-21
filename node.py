00d204607e47874221bba9c3eb5e4ae2502acd2f
lossi1997 <tuukka.orava@outlook.com>
2024-02-21 16:52:54 +0200

    improved readability & typing

--- node.py
+++ node.py
@@ -1,8 +1,6 @@
 
 from typing import Any
 
-import node
-
 
 class Node:
     def __init__(self, value: Any):
@@ -10,35 +8,35 @@ class Node:
         self.next = None
 
     @staticmethod
-    def _get_value(other):
+    def _get_value(other) -> int:
         if isinstance(other, Node):
             return other.value
         else:
             return other
 
-    def __int__(self):
+    def __int__(self) -> int:
         return int(self.value)
 
     def __str__(self) -> str:
         return str(self.value)
 
-    def __add__(self, other):
-        return self.value + self._get_value(other)
-
-    def __sub__(self, other):
-        if other.__rsub__.__name__ == "__rsub__":
-            return self._get_value(other) - self.value
+    def __sub__(self, other) -> int | float:
         return self.value - self._get_value(other)
 
-    def __mul__(self, other):
-        return self.value * self._get_value(other)
+    def __rsub__(self, other) -> int | float:
+        return self._get_value(other) - self.value
 
-    def __truediv__(self, other):
-        if other.__rtruediv__.__name__ == "__rtruediv__":
-            return self._get_value(other) / self.value
+    def __truediv__(self, other) -> float:
         return self.value / self._get_value(other)
 
+    def __rtruediv__(self, other) -> float:
+        return self._get_value(other) / self.value
+
+    def __add__(self, other) -> int | float:
+        return self.value + self._get_value(other)
+
+    def __mul__(self, other) -> int | float:
+        return self.value * self._get_value(other)
+
     __radd__ = __add__
-    __rsub__ = __sub__
     __rmul__ = __mul__
-    __rtruediv__ = __truediv__
